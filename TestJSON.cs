4520fc2d89be5965e42979d3afc6798eb010bdfe
deadmadara <46846194+deadmadara@users.noreply.github.com>
2021-07-18 16:10:17 +0300

    Update TestJSON.cs

--- TestJSON.cs
+++ TestJSON.cs
@@ -25,20 +25,24 @@ namespace SaveDataTest
     {
         static void Main(string[] args)
         {
+            //сериализация объекта в строку формата JSON 
             Player p1 = new Player("Valeros", "Human", "Warrior", 1);
             string JsonString = JsonConvert.SerializeObject(p1);
             Console.WriteLine(JsonString);
 
+            //десериализация строку формата JSON в объект
             Player p2 = JsonConvert.DeserializeObject<Player>(JsonString);
             Console.WriteLine(p2.Name + " " + p2.Race + " " + p2.Class + " " + p2.Level);
 
             JsonSerializer serializer = new JsonSerializer();
 
+            //запись сериализованного объекта в файл d:\json.txt
             StreamWriter StreamWriterExample = new StreamWriter(@"d:\json.txt");
             JsonWriter JsonWriterExample = new JsonTextWriter(StreamWriterExample);
             serializer.Serialize(JsonWriterExample, p1);
             StreamWriterExample.Close();
 
+            //чтение строки из файла d:\json.txt и десериализация в объект
             StreamReader StreamReaderExample = new StreamReader(@"d:\json.txt");
             JsonReader JsonReaderExample = new JsonTextReader(StreamReaderExample);
             Player p3 = serializer.Deserialize<Player>(JsonReaderExample);
